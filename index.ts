52fc61579dbfead27cf8f1ac7f6de207848b4019
Agent-cat <2300031042@kluniversity.in>
2026-03-21 20:51:30 +0530

    update greeting message in root route to reflect version 2

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT ?? 8080;
 
 app.get("/", (req, res) => {
-  res.json({ msg: "Hello from server v1" });
+  res.json({ msg: "Hello from server v2" });
 });
 app.get("/vishnu", (req, res) => {
   res.json({ msg: "Hi vishnu" });
